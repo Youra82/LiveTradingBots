876784eb789f3411d40d822d4760a725ed6cdd9a
Youra82 <matola@outlook.de>
2025-09-03 23:32:05 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,11 @@
+module.exports = {
+  apps : [{
+    name   : "envelope-bot",
+    script : "/home/ubuntu/LiveTradingBots/code/strategies/envelope/run.py",
+    interpreter: "/home/ubuntu/LiveTradingBots/code/.venv/bin/python3",
+    watch: false,
+    autorestart: true,
+    max_memory_restart: '200M'
+  }]
+};
 
